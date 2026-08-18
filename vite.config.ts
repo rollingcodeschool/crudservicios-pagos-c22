6932453c28cd64acca85a8df0fbc1ec75a665d5b
Emilse Arias <emilsearias@gmail.com>
2026-08-18 20:40:42 -0300

    se quito la configuracion de vit ssl y se agregaron los botones para agregar restar y eliminar un servicio del carrito

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,12 @@ import { defineConfig } from 'vite'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
 import tailwindcss from '@tailwindcss/vite'
-import basicSsl from '@vitejs/plugin-basic-ssl'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] }),
-    tailwindcss(),
-    basicSsl()
+    tailwindcss()
   ],
 })
